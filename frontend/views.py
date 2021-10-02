0375d7f3213a637e4097ab1eead046fee7ffd507
FoxHawk <foxhawk42@gmail.com>
2021-10-02 12:26:17 +0100

    Initial setup

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.template import Template, loader
 
 # Create your views here.
+
+def index(request: HttpRequest):
+	tp = loader.get_template("index.html")
+
+	context = {"a":"b"}
+
+	return HttpResponse(tp.render(context=context, request=request))
+
+def manage(request: HttpRequest):
+	pass
