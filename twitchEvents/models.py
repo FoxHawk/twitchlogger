3e1bb22ba75e570685a7f4064ddd4d0eea612a1b
FoxHawk <foxhawk42@gmail.com>
2021-10-02 14:25:52 +0100

    Removed EventEntry table as it is now unneeded

--- twitchEvents/models.py
+++ twitchEvents/models.py
@@ -3,11 +3,7 @@ from django.db import models
 # Create your models here.
 
 class LogEntry(models.Model):
-	channel = models.ForeignKey("EventEntry", on_delete=models.CASCADE)
+	channel = models.CharField(max_length=50)
 	startedAt = models.DateTimeField()
 	eventID = models.IntegerField()
 	type = models.CharField(max_length=20)
-
-class eventEntry(models.Model):
-	id = models.CharField(max_length=30)
-	channel = models.CharField(max_length=50, primary_key=True)
