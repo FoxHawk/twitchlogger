2f9422379dc29c893a65d9183aac4b3d9d7d04d8
FoxHawk <foxhawk42@gmail.com>
2021-10-17 18:43:32 +0100

    Initial Views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
+from django.http.response import HttpResponseBadRequest, HttpResponseServerError
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.template import loader
+import json
+from .. import manageSubscriptions
 
 # Create your views here.
+
+def fetchSubbedEvents(request: HttpRequest):
+	pass
+
+def fetchLogs(request: HttpRequest):
+	pass
