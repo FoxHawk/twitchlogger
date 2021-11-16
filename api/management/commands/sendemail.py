af1bf5237b081f80754b05ee291f03c11cbb4d1e
FoxHawk <foxhawk42@gmail.com>
2021-11-16 19:20:30 +0000

    Corrected sender email address

--- api/management/commands/sendemail.py
+++ api/management/commands/sendemail.py
@@ -5,12 +5,15 @@ from django.utils import timezone
 from datetime import timedelta
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+from email.message import EmailMessage
+import smtplib
+from django.conf import settings
 
 class Command(BaseCommand):
 	help = "Sends a notification email with all the streams that started today"
 
 	def handle(self, *args, **options):
-		logs = LogEntry.objects.filter(startedAt__gte=timezone.now().replace(hour=0, minute=0, second=0) - timedelta(days=2))
+		logs = LogEntry.objects.filter(startedAt__gte=timezone.now().replace(hour=0, minute=0, second=0))
 
 		if len(logs) == 0:
 			return
@@ -26,6 +29,5 @@ class Command(BaseCommand):
 		
 		html_message = render_to_string("mail.html", context=context)
 		plain_message = strip_tags(html_message)
-		#print(html_message)
-		send_mail("Twitch Streams From Today", plain_message, "TwitchLog <twitchlog@foxhawk.co.uk>", ["fox@foxhawk.co.uk"], html_message=html_message)
 		
+		send_mail("Twitch Streams From Today", plain_message, "TwitchLog <twitchlogs@foxhawk.co.uk>", ["fox@foxhawk.co.uk"], html_message=html_message)
