75d6495d31d36663cf1f25459b750114a5b86e98
FoxHawk <foxhawk42@gmail.com>
2022-02-13 14:25:05 +0000

    Added comments

--- frontend/views.py
+++ frontend/views.py
@@ -10,12 +10,13 @@ from twitchEvents.models import LogEntry
 
 def index(request: HttpRequest):
 	channels = None
+	#if the from date or to date were supplied, use the default values of the beginning of the day to now
 	if "fromDate" not in request.GET.keys() or "toDate" not in request.GET.keys():
 		channels = getStreamers(timezone.now().replace(hour=0, minute=0, second=0, microsecond=0), timezone.now())
 	else:
 		channels = getStreamers(request.GET["fromDate"], request.GET["toDate"])
 
-
+	#load the template and render it with the channels in the context
 	tp = loader.get_template("index.html")
 	return HttpResponse(tp.render(request=request, context={"streamers": channels}))
 
@@ -23,10 +24,11 @@ def getStreamers(dateFrom: timezone, dateTo: timezone):
 	data = LogEntry.objects.filter(datetimestamp__gte=dateFrom, datetimestamp__lte=dateTo).exclude(game="N/A").exclude(game="")
 	channels = []
 
+	#create a list entry for every unique channel
 	for i in data.values("channel").distinct():
 		channels.append({"name": i["channel"], "games": []})
 	
-	for i in channels:
+	for i in channels: #fill out the games that were played between the datetime durations
 		for j in data.filter(channel=i["name"]).values("datetimestamp", "game"):
 			i["games"].append({"timestamp": j["datetimestamp"], "game": j["game"]})
 	
