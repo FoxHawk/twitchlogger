86bc0d84e8a4aecc9a4b76328b76b78a0a249b9a
FoxHawk <foxhawk42@gmail.com>
2022-02-13 13:28:04 +0000

    Added logic for new frontend

--- frontend/views.py
+++ frontend/views.py
@@ -1,14 +1,38 @@
+from datetime import timedelta
+from django.utils import timezone
 from django.http.response import HttpResponseBadRequest, HttpResponseServerError
 from django.http import HttpRequest, HttpResponse
 from django.template import loader
-import requests
 from api import manageSubscriptions
+from twitchEvents.models import LogEntry
 
 # Create your views here.
 
 def index(request: HttpRequest):
+	channels = None
+	if "fromDate" not in request.GET.keys() or "toDate" not in request.GET.keys():
+		channels = getStreamers(timezone.now().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=7), timezone.now())
+	else:
+		channels = getStreamers(request.GET["fromDate"], request.GET["toDate"])
+
+
 	tp = loader.get_template("index.html")
-	return HttpResponse(tp.render(request=request))
+	return HttpResponse(tp.render(request=request, context={"streamers": channels}))
+
+def getStreamers(dateFrom: timezone, dateTo: timezone):
+	data = LogEntry.objects.filter(datetimestamp__gte=dateFrom, datetimestamp__lte=dateTo)
+	channels = []
+
+	for i in data.values("channel").distinct():
+		channels.append({"name": i["channel"], "games": []})
+	
+	for i in channels:
+		for j in data.filter(channel=i["name"]).values("datetimestamp", "game"):
+			if j["game"] == "N/A" or j["game"] == "":
+				continue
+			i["games"].append({"timestamp": j["datetimestamp"], "game": j["game"]})
+	
+	return channels
 
 def manage(request: HttpRequest):
 	if (request.method == "POST"):
