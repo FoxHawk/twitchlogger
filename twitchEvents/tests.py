19b8636b2f77187feffb20b4e4eb5a2466fc4b6b
FoxHawk <foxhawk42@gmail.com>
2022-04-09 17:53:13 +0100

    added test for the getLogEntry function

--- twitchEvents/tests.py
+++ twitchEvents/tests.py
@@ -76,10 +76,13 @@ class endpointDuplicateRequestTests(TestCase):
 		return response
 
 class endpointGetLogEntryTests(TestCase):
-	def setUp(self):
-		pass
-
 	def test_get_LogEntry(self):
-		pass
+		request = {"subscription":{"id":"f1c2a387-161a-49f9-a165-0f21d7a4e1c4","type":"stream.online","version":"1","status":"enabled","cost":0,"condition":{"broadcaster_user_id":"113954840"},"transport":{"method":"webhook","callback":"https://example.com/webhooks/callback"},"created_at":"2019-11-16T10:11:12.123Z"},"event":{"id":"9001","broadcaster_user_id":"12826","broadcaster_user_login":"cool_user","broadcaster_user_name":"Cool_User","type":"live","started_at":"2020-10-11T10:11:12.123Z"}}
+		header = {"Twitch-Eventsub-Message-Id": "test"}
+
+		log = views.getLogEntry(request, header)
+
+		self.assertEqual(log.eventid, "test")
+		self.assertEqual(log.type, "stream.online")
 
 
